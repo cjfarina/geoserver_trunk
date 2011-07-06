8be6987427b701f35f547ed17702fa2541c4a66e
Carlos Farina <cfarina@xoomcode.com>
2011-07-05 23:31:01 -0300

    Changes in FilterChainDecoder.

--- community/geosync/src/main/java/org/geoserver/gss/internal/atom/decoders/filters/FilterChainDecoder.java
+++ community/geosync/src/main/java/org/geoserver/gss/internal/atom/decoders/filters/FilterChainDecoder.java
@@ -5,22 +5,39 @@ import java.io.IOException;
 import javax.xml.namespace.QName;
 
 import org.geoserver.gss.internal.atom.decoders.AbstractDecoder;
+import org.geotools.filter.FilterFactory;
+import org.geotools.filter.FilterFactoryFinder;
 import org.geotools.filter.v1_1.OGC;
 import org.gvsig.bxml.stream.BxmlStreamReader;
 import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.Id;
+
+import static org.geotools.filter.v1_1.OGC.FeatureId;
 
 public class FilterChainDecoder extends AbstractDecoder<Filter> {
 
-    private FeatureIdFilterDecoder filterLink;
+    private AndFilterDecoder filterLink;
 
     private Filter filter;
 
     public FilterChainDecoder() {
         super(OGC.Filter);
+        filterLink = new AndFilterDecoder();
     }
 
     protected void decodeElement(final BxmlStreamReader r) throws IOException {
-        filter = filterLink.decodeFilter(r);
+        QName name = r.getElementName();
+        
+        if(FeatureId.equals(name)){
+            /*if(filter == null){
+                FilterFactory ff = FilterFactoryFinder.createFilterFactory();
+                filter = ff.createFidFilter();
+            }
+            ((Id)filter).accept(paramFilterVisitor, paramObject)*/
+        } else {
+            filter = filterLink.decodeFilter(r);
+        }
     }
 
     @Override
