d1c3a6c17f804263d455958ac5fab6d702e63aa8
Gabriel Roldan <gabriel.roldan@gmail.com>
2011-08-15 00:15:45 -0300

    GeoGitDataStore, first draft

---
+++ community/geogit-versioning/src/main/java/org/geoserver/data/geogit/GeoGitFeatureSource.java
@@ -0,0 +1,181 @@
+package org.geoserver.data.geogit;
+
+import java.awt.RenderingHints.Key;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import org.geotools.data.DefaultResourceInfo;
+import org.geotools.data.FeatureListener;
+import org.geotools.data.Query;
+import org.geotools.data.QueryCapabilities;
+import org.geotools.data.ResourceInfo;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.store.EmptyFeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.Name;
+import org.opengis.filter.Filter;
+import org.opengis.filter.sort.SortBy;
+
+import com.google.common.base.Throwables;
+
+public class GeoGitFeatureSource implements SimpleFeatureSource {
+
+    private SimpleFeatureType type;
+
+    private final GeoGitDataStore dataStore;
+
+    public GeoGitFeatureSource(final SimpleFeatureType type, final GeoGitDataStore dataStore) {
+        this.type = type;
+        this.dataStore = dataStore;
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getName()
+     */
+    @Override
+    public Name getName() {
+        return type.getName();
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getInfo()
+     */
+    @Override
+    public ResourceInfo getInfo() {
+        DefaultResourceInfo info = new DefaultResourceInfo();
+        ReferencedEnvelope bounds;
+        try {
+            bounds = getBounds();
+            if (bounds != null) {
+                info.setBounds(bounds);
+                info.setCRS(bounds.getCoordinateReferenceSystem());
+            }
+        } catch (IOException e) {
+            Throwables.propagate(e);
+        }
+        info.setName(getName().getLocalPart());
+        info.setDescription("GeoGit backed Feature Source");
+        return info;
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getDataStore()
+     */
+    @Override
+    public GeoGitDataStore getDataStore() {
+        return dataStore;
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getQueryCapabilities()
+     */
+    @Override
+    public QueryCapabilities getQueryCapabilities() {
+        QueryCapabilities caps = new QueryCapabilities() {
+            /**
+             * @see org.geotools.data.QueryCapabilities#isUseProvidedFIDSupported()
+             * @return {@code true}
+             */
+            @Override
+            public boolean isUseProvidedFIDSupported() {
+                return true;
+            }
+
+            /**
+             * @see org.geotools.data.QueryCapabilities#supportsSorting(org.opengis.filter.sort.SortBy[])
+             * @return {@code false}
+             */
+            @Override
+            public boolean supportsSorting(SortBy[] sortAttributes) {
+                return false;
+            }
+        };
+
+        return caps;
+    }
+
+    @Override
+    public void addFeatureListener(FeatureListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeFeatureListener(FeatureListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getSchema()
+     */
+    @Override
+    public SimpleFeatureType getSchema() {
+        return type;
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getBounds()
+     * @see #getBounds(Query) getBounds(Query.ALL)
+     */
+    @Override
+    public ReferencedEnvelope getBounds() throws IOException {
+        return getBounds(Query.ALL);
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getBounds(org.geotools.data.Query)
+     */
+    @Override
+    public ReferencedEnvelope getBounds(final Query query) throws IOException {
+        // TODO optimize, please
+        SimpleFeatureCollection features = getFeatures(query);
+        ReferencedEnvelope bounds = features.getBounds();
+        return bounds;
+    }
+
+    @Override
+    public int getCount(Query query) throws IOException {
+        // TODO optimize, please
+        SimpleFeatureCollection features = getFeatures(query);
+        int size = features.size();
+        return size;
+    }
+
+    /**
+     * @see org.geotools.data.FeatureSource#getSupportedHints()
+     */
+    @Override
+    public Set<Key> getSupportedHints() {
+        return Collections.emptySet();
+    }
+
+    /**
+     * @see org.geotools.data.simple.SimpleFeatureSource#getFeatures()
+     */
+    @Override
+    public SimpleFeatureCollection getFeatures() throws IOException {
+        return getFeatures(Query.ALL);
+    }
+
+    @Override
+    public SimpleFeatureCollection getFeatures(final Filter filter) throws IOException {
+        return getFeatures(new Query(getName().getLocalPart(), filter));
+    }
+
+    @Override
+    public SimpleFeatureCollection getFeatures(final Query query) throws IOException {
+        Filter filter = query.getFilter();
+        if (filter == null) {
+            filter = Filter.INCLUDE;
+        }
+        if (Filter.EXCLUDE.equals(filter)) {
+            return new EmptyFeatureCollection(type);
+        }
+        return new GeoGitSimpleFeatureCollection(type, filter, dataStore.getRepository());
+    }
+
+}
---
+++ community/geogit-versioning/src/main/java/org/geoserver/data/geogit/GeoGitSimpleFeatureCollection.java
@@ -0,0 +1,447 @@
+package org.geoserver.data.geogit;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.geogit.api.ObjectId;
+import org.geogit.api.Ref;
+import org.geogit.api.RevTree;
+import org.geogit.api.SpatialRef;
+import org.geogit.repository.Repository;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.CollectionListener;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
+import org.opengis.feature.Feature;
+import org.opengis.feature.FeatureVisitor;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.Name;
+import org.opengis.filter.Filter;
+import org.opengis.filter.sort.SortBy;
+import org.opengis.filter.spatial.BBOX;
+import org.opengis.geometry.BoundingBox;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.util.ProgressListener;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Throwables;
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Iterators;
+
+public class GeoGitSimpleFeatureCollection implements SimpleFeatureCollection {
+
+    private final SimpleFeatureType type;
+
+    private final Filter filter;
+
+    private final Repository repo;
+
+    private Set<SimpleFeatureIterator> openIterators;
+
+    public GeoGitSimpleFeatureCollection(final SimpleFeatureType type, final Filter filter,
+            final Repository repo) {
+        this.type = type;
+        this.filter = filter;
+        this.repo = repo;
+        this.openIterators = new CopyOnWriteArraySet<SimpleFeatureIterator>();
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#getSchema()
+     */
+    @Override
+    public SimpleFeatureType getSchema() {
+        return type;
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#getID()
+     */
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#purge()
+     */
+    @Override
+    public void purge() {
+        for (Iterator<SimpleFeatureIterator> it = openIterators.iterator(); it.hasNext();) {
+            SimpleFeatureIterator next = it.next();
+            it.remove();
+            next.close();
+        }
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#getBounds()
+     */
+    @Override
+    public ReferencedEnvelope getBounds() {
+        final Name typeName = type.getName();
+        final Ref thisTypeTreeRootRef = repo.getRootTreeChild(getTypePath(typeName));
+
+        final CoordinateReferenceSystem crs = type.getCoordinateReferenceSystem();
+
+        ReferencedEnvelope bounds = new ReferencedEnvelope(crs);
+
+        if (null == thisTypeTreeRootRef) {
+            return bounds;
+        }
+
+        final RevTree typeTree = repo.getTree(thisTypeTreeRootRef.getObjectId());
+        Preconditions.checkState(typeTree != null);
+
+        final FeatureRefIterator refs = new FeatureRefIterator(typeName, filter, repo);
+
+        BoundingBox featureBounds;
+        if (refs.isFullySupported()) {
+            while (refs.hasNext()) {
+                Ref ref = refs.next();
+                if (ref instanceof SpatialRef) {
+                    SpatialRef sp = (SpatialRef) ref;
+                    featureBounds = sp.getBounds();
+                    expandToInclude(bounds, featureBounds);
+                }
+            }
+        } else {
+            Iterator<SimpleFeature> features = new GeoGitFeatureIterator(refs, type, filter, repo);
+            while (features.hasNext()) {
+                featureBounds = features.next().getBounds();
+                expandToInclude(bounds, featureBounds);
+            }
+        }
+
+        return bounds;
+    }
+
+    private void expandToInclude(ReferencedEnvelope bounds, BoundingBox featureBounds) {
+        final CoordinateReferenceSystem crs = type.getCoordinateReferenceSystem();
+        final CoordinateReferenceSystem featureCrs = featureBounds.getCoordinateReferenceSystem();
+        if (!CRS.equalsIgnoreMetadata(crs, featureCrs)) {
+            try {
+                featureBounds = featureBounds.toBounds(crs);
+            } catch (TransformException e) {
+                Throwables.propagate(e);
+            }
+        }
+        bounds.include(featureBounds);
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#size()
+     */
+    @Override
+    public int size() {
+        final Name typeName = type.getName();
+        final Ref thisTypeTreeRootRef = repo.getRootTreeChild(getTypePath(typeName));
+        if (null == thisTypeTreeRootRef) {
+            return 0;
+        }
+        final RevTree typeTree = repo.getTree(thisTypeTreeRootRef.getObjectId());
+        Preconditions.checkState(typeTree != null);
+
+        if (Filter.INCLUDE.equals(filter)) {
+            final BigInteger size = typeTree.size();
+            return size.intValue();
+        }
+
+        final FeatureRefIterator refs = new FeatureRefIterator(typeName, filter, repo);
+        int size;
+        if (refs.isFullySupported()) {
+            size = Iterators.size(refs);
+        } else {
+            Iterator<SimpleFeature> features = new GeoGitFeatureIterator(refs, type, filter, repo);
+            size = Iterators.size(features);
+        }
+
+        return size;
+    }
+
+    private static List<String> getTypePath(final Name name) {
+        String namespaceURI = name.getNamespaceURI();
+        if (null == namespaceURI) {
+            namespaceURI = "";
+        }
+        String localPart = name.getLocalPart();
+
+        return Arrays.asList(namespaceURI, localPart);
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#iterator()
+     */
+    @Override
+    public Iterator<SimpleFeature> iterator() {
+        final Name typeName = type.getName();
+        final Ref thisTypeTreeRootRef = repo.getRootTreeChild(getTypePath(typeName));
+        if (null == thisTypeTreeRootRef) {
+            return Iterators.emptyIterator();
+        }
+        final RevTree typeTree = repo.getTree(thisTypeTreeRootRef.getObjectId());
+        Preconditions.checkState(typeTree != null);
+
+        final FeatureRefIterator refs = new FeatureRefIterator(typeName, filter, repo);
+        Iterator<SimpleFeature> features = new GeoGitFeatureIterator(refs, type, filter, repo);
+        return features;
+    }
+
+    /**
+     * @see org.geotools.data.simple.SimpleFeatureCollection#features()
+     * @see #iterator()
+     */
+    @Override
+    public SimpleFeatureIterator features() {
+        final Iterator<SimpleFeature> iterator = iterator();
+        return new SimpleFeatureIterator() {
+
+            @Override
+            public SimpleFeature next() throws NoSuchElementException {
+                return iterator.next();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public void close() {
+                // nothing to do?
+            }
+        };
+    }
+
+    /**
+     * @author groldan
+     * 
+     */
+    private static class BBOXPredicate implements Predicate<Ref> {
+
+        private final BBOX filter;
+
+        public BBOXPredicate(final BBOX filter) {
+            this.filter = filter;
+        }
+
+        @Override
+        public boolean apply(final Ref featureRef) {
+            if (!(featureRef instanceof SpatialRef)) {
+                return false;
+            }
+            SpatialRef sr = (SpatialRef) featureRef;
+            BoundingBox bounds = sr.getBounds();
+            final boolean apply = filter.evaluate(bounds);
+            return apply;
+        }
+    }
+
+    /**
+     * @author groldan
+     * 
+     */
+    private static class FeatureRefIterator extends AbstractIterator<Ref> {
+
+        private final Iterator<Ref> refs;
+
+        private final Filter filter;
+
+        public FeatureRefIterator(final Name typeName, final Filter filter, final Repository repo) {
+            this.filter = filter;
+
+            final Ref thisTypeTreeRootRef = repo.getRootTreeChild(getTypePath(typeName));
+            if (null == thisTypeTreeRootRef) {
+                refs = Iterators.emptyIterator();
+                return;
+            }
+
+            final RevTree typeTree = repo.getTree(thisTypeTreeRootRef.getObjectId());
+            Preconditions.checkState(typeTree != null);
+
+            if (Filter.INCLUDE.equals(filter)) {
+                refs = typeTree.iterator(null);
+                return;
+            }
+            if (filter instanceof BBOX) {
+                refs = typeTree.iterator(new BBOXPredicate((BBOX) filter));
+                return;
+            }
+
+            // can't optimize here
+            refs = typeTree.iterator(null);
+        }
+
+        public boolean isFullySupported() {
+            return Filter.INCLUDE.equals(filter) || filter instanceof BBOX;
+        }
+
+        @Override
+        protected Ref computeNext() {
+            if (!refs.hasNext()) {
+                return endOfData();
+            }
+            return refs.next();
+        }
+
+    }
+
+    private static class GeoGitFeatureIterator extends AbstractIterator<SimpleFeature> {
+
+        private final Iterator<Ref> featureRefs;
+
+        private final SimpleFeatureType type;
+
+        private final Repository repo;
+
+        private final Filter filter;
+
+        public GeoGitFeatureIterator(final Iterator<Ref> featureRefs, final SimpleFeatureType type,
+                final Filter filter, final Repository repo) {
+            this.featureRefs = featureRefs;
+            this.type = type;
+            this.filter = filter;
+            this.repo = repo;
+
+        }
+
+        @Override
+        protected SimpleFeature computeNext() {
+            while (featureRefs.hasNext()) {
+                Ref featureRef = featureRefs.next();
+                String featureId = featureRef.getName();
+                ObjectId contentId = featureRef.getObjectId();
+                Feature feature = repo.getFeature(type, featureId, contentId);
+                if (filter.evaluate(feature)) {
+                    return (SimpleFeature) feature;
+                }
+            }
+            return endOfData();
+        }
+
+    }
+
+    @Override
+    public void accepts(FeatureVisitor visitor, ProgressListener progress) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void close(FeatureIterator<SimpleFeature> close) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void close(Iterator<SimpleFeature> close) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#addListener(org.geotools.feature.CollectionListener)
+     */
+    @Override
+    public void addListener(CollectionListener listener) throws NullPointerException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#removeListener(org.geotools.feature.CollectionListener)
+     */
+    @Override
+    public void removeListener(CollectionListener listener) throws NullPointerException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @see org.geotools.feature.FeatureCollection#isEmpty()
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean add(SimpleFeature obj) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends SimpleFeature> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(
+            FeatureCollection<? extends SimpleFeatureType, ? extends SimpleFeature> resource) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <O> O[] toArray(O[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SimpleFeatureCollection subCollection(Filter filter) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SimpleFeatureCollection sort(SortBy order) {
+        throw new UnsupportedOperationException();
+    }
+
+}
